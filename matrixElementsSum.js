c9da2d20df2b0d2b20d4c7a6dd512ef21ba3df23
Isaac Yoon <ysaacioon@yahoo.com>
2017-03-27 20:43:34 -0700

    complete reverseParentheses

--- matrixElementsSum.js
+++ matrixElementsSum.js
@@ -46,10 +46,74 @@ function allLongestStrings2(inputArray) {
     console.log('maxSize', maxSize)
     return inputArray.filter(x => x.length === maxSize);
 }
-console.log(allLongestStrings2(["aba", "aa", "ad", "vcd", "aba"]))
+// console.log(allLongestStrings2(["aba", "aa", "ad", "vcd", "aba"]))
 
 
 
 function commonCharacterCount(s1, s2) {
-
+	var count = 0;
+	for (var i = 0; i <s1.length; i++) {
+		for (var j =0 ; j<s2.length; j++) {
+			if(s1[i] === s2[j]){
+				count++;
+				s2 = s2.split('');
+				s2.splice(j,1);
+				s2= s2.join('')
+				break;
+			}
+		}
+	}
+	return count;
 }
+var s1, s2;
+s1 = "aabcc";
+s2 = "adcaa";
+// console.log(commonCharacterCount(s1, s2))
+
+function isLucky(n) {
+	t = n.toString().split('')
+	n = t.splice(0, t.length/2)
+	n2 = t.splice(t.length/2 -1);
+	return n.reduce((x,y) => parseInt(x)+parseInt(y)) === n2.reduce((x,y) => parseInt(x)+parseInt(y))
+}
+
+isLucky(1230)
+isLucky(239017)
+
+
+function sortByHeight(a) {
+	var negative = [];
+	for(var i = 0 ; i < a.length; i ++) {
+		if(a[i] === -1) {
+			negative.push(i);
+		}
+	}
+	for (var i = 0; i < a.length; i ++) {
+		if(a[i] === -1) {
+			a.splice(i, 1)
+			i--
+		}
+	}
+	a = a.sort((x,y) => x-y)
+	for(var i = 0; i < negative.length; i++) {
+		a.splice(negative[i], 0,  -1 )
+	}
+	console.log(a)
+	console.log(negative)
+	return a;
+}
+
+function sortByHeight2(a) {
+    var s = a.filter(h => h > 0).sort((a, b) => a - b)
+    console.log(s)
+    return a.map(p => {
+        if (p !== -1) {
+            return s.shift();
+        }
+
+        return -1;
+    })
+}
+
+
+sortByHeight2([-1, 150, 190, 170, -1, -1, 160, 180])
