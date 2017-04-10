daf2d3990a5a5bc3a05ddceac86bc5b461e4a3fd
Isaac Yoon <ysaacioon@yahoo.com>
2017-04-09 21:25:47 -0700

    complete HeightOfBinaryTree

--- HeighOfBinaryTree.js
+++ HeighOfBinaryTree.js
@@ -82,3 +82,44 @@ let heightOfBinaryTree = function(tree) {
 // }
 
 console.log(heightOfBinaryTree(tree))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let binaryTree = function(val) {
+	this.val = val;
+	this.left = null;
+	this.right = null;
+}
+
+binaryTree.prototype.insert = function(val) {
+	if(val < this.val) {
+		if(this.left === null) {
+			this.left = new binaryTree(val)
+		} else {
+			this.left.insert(val)
+		}
+	} else {
+		if(this.right === null){
+			this.right = new binaryTree(val)
+		} else {
+			this.right.insert(val);
+		}
+	}
+}
+
+var tree = new binaryTree(3);
+tree.insert(2);
+tree.insert(5);
+tree.insert(1);
+console.log(tree);
