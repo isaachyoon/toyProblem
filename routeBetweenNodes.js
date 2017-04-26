2feeea6e5f6d89e2ab889115bf61e8b5db194844
Isaac Yoon <ysaacioon@yahoo.com>
2017-04-26 16:04:12 -0700

    complete routeBetweenNodes

---
+++ routeBetweenNodes.js
@@ -0,0 +1,34 @@
+var graph = {
+	'1': [4,2],
+	'2': [1,5,3],
+	'3': [2,4],
+	'5': [2],
+	'4': [1,3]
+}
+
+var routeBetweenNodes = function(obj, node1, node2) {
+	var visited = [];
+	result = false;
+	function findNode(node1){
+		console.log('visited ', node1)
+		if(visited.indexOf(node1) === -1) {
+			visited.push(node1);
+		}
+		if(node1 === node2){
+			result = true;
+			return;
+		}
+		for(var i=0; i < obj[node1].length; i ++) {
+			if(visited.indexOf(obj[node1][i]) === -1){
+				findNode(obj[node1][i])
+			}
+		}
+	}
+	findNode(node1)
+	return result;
+}
+
+console.log(routeBetweenNodes(graph, 4, 2))
+
+
+
