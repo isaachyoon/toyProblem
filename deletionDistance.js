17097b4809d0de82cae435c9d7f09e412f435b27
Isaac Yoon <ysaacioon@yahoo.com>
2017-06-15 15:56:56 -0700

    deletionDistance

--- deletionDistance.js
+++ deletionDistance.js
@@ -1,11 +1,11 @@
 function deletionDistance(str1, str2){
-	//step 1. create an matrix that is 1 size bigger
+	//step 1. create an matrix that is 1 size bigger (I used a helper function to create a quick grid)
 	var matrix = createMatrix(str1.length+1 , str2.length+1)
 
 
 	for(var i = 0; i < matrix.length; i++) {
 		for(var j = 0; j < matrix[i].length; j ++) {
-			/////2. I am going to make a grid with numbers
+			/////2. I am going to set the grid with numbers
 			if (i === 0) {
 				matrix[i][j] = j;
 			} else if (j === 0) {
@@ -41,9 +41,23 @@ function deletionDistance(str1, str2){
 		        f r o g
 					0 1 2 3 4
 				d	1 2 3 4 5
-				o	2
+				o	2 3 4 *     -> if the intersected numbers are same, then you take the number directly across [i-1][j-1] and
+				g	3              set it to that value. in this case is 3.
+
+
+				    f r o g
+					0 1 2 3 4
+				d	1 2 3 4 5
+				o	2 3 4 3 *    -> 'g' and 'o' aren't same so take the smaller the number immediate to the top and left (5 vs 3) and ADD ONE
 				g	3
 
+				    f r o g
+					0 1 2 3 4
+				d	1 2 3 4 5
+				o	2 3 4 3 4
+				g	3 4 5 4 3    return the value at the end of matrix.
+
+				OVERALL IDEA: MATRIX KEEP TRACK OF DIFFERENCE IN CHARACTER - EX.('FR' vs 'D' has a difference in character of 3)
 			*/
 		}
 
