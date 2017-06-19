292e1a18e5beb8431f0f9406a6500303965c4235
Isaac Yoon <ysaacioon@yahoo.com>
2017-06-19 12:33:10 -0700

    robotPath

--- robotPath.js
+++ robotPath.js
@@ -1,42 +1,49 @@
-function robotPath(matrix, r, c) {
-	console.log(matrix)
-	console.log('r: ' + r + " c: " + c )
-	r = r || 1;
-	c = c || 1;
-	if(r === matrix.length -1 && c === matrix.length -1) {
-		return matrix[r][c];
-	}
-	if(r < 0 || c < 0 || r > matrix.length-1 || c > matrix.length-1) {
+/*
+KEY FINDING:
+1. IF THE RECUSIVE FUNCTION IS NOT GREAT ENOUGH, IT WILL TAKE LONGER BECUASE YOU HAVE TO STORE INTO MEMORY AS YOU CACHE RESULTS.
+2. THERE IS A WAY TO DO IT ITERATIVELY WITHOUT USING DYNAMIC PROGRAMMING. IT ESSENTIALLY USES THE IDEA OF DYNAMIC PROBLEMING AND STATICALLY APPLIES IT
+3. MAKE SURE TO RETURN 0 RATHER THAN RETURN BECAUSE IN THIS CASE, THE NUMBERS ARE ADDED UP
+
+
+*/
+
+function robotPath(matrix, r, c, memo) {
+	var key = JSON.stringify(r + ':' + c)
+	// console.log(key)
+
+	if(r === matrix.length-1 && c === matrix.length - 1) {
+		// console.log('reached')
+		return 1;
+	} else if (r >= matrix.length || c >= matrix.length) {
 		return 0;
-	} else if(r === 0 && c === 0) {
-		matrix[r][c] = 1;
-	} else if(r === 0 || c === 0) {
-		matrix[r][c] = 1;
-	} else if(matrix[r][c] !== 1) {
-			matrix[r][c] = matrix[r-1][c] + matrix[r][c-1]
 	}
-		return robotPath(matrix, r+1, c) && robotPath(matrix, r, c+1)
-
-
+	else if (matrix[r][c] === 'X'){
+		return 0;
+	}
+	// var output = (robotPath(matrix, r+1, c, memo) + robotPath(matrix,r,c+1, memo))
+	// return output
+	else if (!memo[key]) {
+		memo[key] = (robotPath(matrix, r+1, c, memo) + robotPath(matrix,r,c+1, memo))
+	}
+	return memo[key]
 }
 
-function createGrid(n) {
-	let matrix = []
-	for(var i = 0; i < n; i++) {
-		matrix[i] = [];
-		for(var j = 0; j < n; j++) {
-			if(i === 0 || j === 0) {
-				matrix[i][j] = 1
-			} else {
-				matrix[i][j] = -1;
-			}
-		}
-	}
-	return matrix;
-}
 
-var grid = createGrid(4)
+var grid = [
+	['-1','-1','-1','-1','-1','-1'],
 
-var output = robotPath(grid)
-console.log(output);
+	['-1','X','-1','-1','-1','-1'],
+
+	['-1','-1','-1','-1','X','-1'],
+
+	['-1','-1','X','-1','-1','-1'],
+
+	['-1','-1','-1','X','-1','-1'],
+
+	['-1','X','-1','-1','-1','-1']
+]
+
+var output = robotPath(grid, 0, 0, {})
+console.log(output)
+
 
